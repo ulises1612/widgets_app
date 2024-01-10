554b3c1f8b3a4572e17537a97e4329c2c112e959
Ulises Montoya <ulisesjafetmontoya@gmail.com>
2024-01-09 20:58:55 -0600

    go router terminado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -51,8 +52,8 @@ class _CustomListTile extends StatelessWidget {
       trailing: Icon(Icons.arrow_forward_ios_rounded,color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
-        //TODO navegar a otra pantalla
+      onTap: (){ 
+        context.push(menuItem.link);
       },
     );
   }
